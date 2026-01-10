c71722904fe5223b8852dbf15258cde2e4b93520
yelnats7 <stanleyomula7@gmail.com>
2026-01-10 21:47:42 +0300

    Made my fisrt branch

--- date_calculator/time-till-deadline.py
+++ date_calculator/time-till-deadline.py
@@ -1,24 +1,21 @@
-from datetime import datetime, date
+import datetime
 
 def main():
-    goal = input("Enter your goal with a deadline sepaarated by a colon\n")
-    deadline = goal.split(":")
+    try:
+        user_input = input("Enter your goal and the date (Goal:dd/mm/yyyy):\n").split(":")
+        
+        goal = user_input[0]
+        deadline = user_input[1].strip()
 
-    deadline_date = deadline[1]
-    current_date = str(date.today().strftime("%d/%m/%Y"))
+        deadline_date = datetime.datetime.strptime(deadline, "%d/%m/%Y")
+        current_date = datetime.datetime.now()
 
-    dates = [deadline_date, current_date]
+        days_left = (deadline_date - current_date).days
+        print(f"You have {days_left} days left to achieve: {goal}")
 
-    #format_code = "%d/%m/%Y"
-    #deadline_date_object = datetime.strptime(deadline_date, format_code)
-    datees = {"date1": None, "date2": None}
-    key_list =["date1", "date2"]
+    except IndexError:
+        print("Error: You forgot to separate the goal and date with a colon (:).")
+    except ValueError:
+        print("Error: The date format should be Day/Month/Year (e.g., 31/12/2025).")
 
-    def format_date(date_given):
-        return date(int(date_given.split("/")[2]), int(date_given.split("/")[1]), int(date_given.split("/")[0]))
-    for i in range(2):
-        current_key = key_list[i]
-        datees[current_key] = format_date(dates[i])
-
-    print(f"{(datees["date1"] - datees["date2"]).days} days")
 main()
